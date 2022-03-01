19ed1edee12dd2878c5d694efc1c4dab365f8c60
papguru <94305074+papguru@users.noreply.github.com>
2022-03-01 14:48:15 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+
+$(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if (height > 100) {
+            $('#back2Top').fadeIn();
+        } else {
+            $('#back2Top').fadeOut();
+        }
+    });
+    $(document).ready(function() {
+        $("#back2Top").click(function(event) {
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+            return false;
+        });
+    var nextURL;
+function updateNextURL( doc ) {
+  nextURL = $( doc ).find('.prathyoush__page-next').attr('href');
+  
+}
+updateNextURL( document );
+var $container = $('.container').infiniteScroll({
+  path: function() {
+    return nextURL;
+  },
+ 
+  append: '.prath_cont',
+  history: 'push',
+  historyTitle: true,
+  status: '.page-load-status',
+  checkLastPage: '.prathyoush__page-next'
+ 
+});
+$container.on( 'load.infiniteScroll', function( event, response, path ) {
+  updateNextURL( response );
+});
+    });

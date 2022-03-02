3c38e122c479eabd104f0b5a888227b840ad4a6b
papguru <94305074+papguru@users.noreply.github.com>
2022-03-02 12:42:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -56,22 +56,6 @@ $container.on( 'load.infiniteScroll', function( event, response, path ) {
 
     });
 
-$(document).ready(function(){
-    $(".prathyoush-face").click(function(event){
-    var $tempElement = $("<input>");
-        $("body").append($tempElement);
-        $tempElement.val($(this).closest(".prathyoush-face").find(".prathyoush-copy").text()).select();
-        document.execCommand("Copy");
-        $tempElement.remove();
-       
-    });
-    $(".prathyoush-face").click(function () {
-        $("#prathyoush-alert").show().delay(100).fadeOut();        
-      
-            });
-	
 
-	
-});
 
 	
